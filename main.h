d7064628dee59140a4f4cab1534c2303e001d671
Othman Rhajbal <othman.rhajbal00@gmail.com>
2023-11-13 18:08:47 -0800

    0x11 C printf

--- main.h
+++ main.h
@@ -1,11 +1,8 @@
-#ifndef MAIN_H
-#define MAIN_H
-
+#ifndef HEADER_FILE
+#define HEADER_FILE
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
-#include <unistd.h>
-#include <limits.h>
 
 typedef struct formats
 {
@@ -20,15 +17,5 @@ int _percent(va_list list);
 int _char(va_list list);
 int _string(va_list list);
 int _integer(va_list list);
-int printf_char(va_list args, int printed);
-int printf_string(va_list args, int printed);
-int printf_integer(va_list args, int printed);
-int selector(const char *format, va_list args, int printed);
-int printf_binary(unsigned int num, int printed);
-int _x(unsigned int num, int printed, int uppercase);
-int printf_octal(unsigned int num, int printed);
-int printf_unsigned(unsigned int num, int printed);
-int printf_reverse(va_list args, int printed);
-int printf_pointer(va_list args, int printed);
 
 #endif
