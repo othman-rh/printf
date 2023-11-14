a6d77e182799591413ec7094a01661a5f579abe9
Othman Rhajbal <othman.rhajbal00@gmail.com>
2023-11-13 17:05:17 -0800

    0x11 C printf ADVANcED

--- main.h
+++ main.h
@@ -1,22 +1,21 @@
-#ifndef MAIN_H
-#define MAIN_H
-
+#ifndef HEADER_FILE
+#define HEADER_FILE
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdarg.h>
-#include <unistd.h>
-#include <limits.h>
 
-int _putchar(char character);
-int printf_char(va_list args, int printed);
-int printf_string(va_list args, int printed);
-int printf_integer(va_list args, int printed);
-int selector(const char *format, va_list args, int printed);
-int printf_binary(unsigned int num, int printed);
+typedef struct formats
+{
+	char *sym;
+	int (*f)(va_list);
+} fmtsSpefier;
+
+int _putchar(char c);
 int _printf(const char *format, ...);
-int _x(unsigned int num, int printed, int uppercase);
-int printf_octal(unsigned int num, int printed);
-int printf_unsigned(unsigned int num, int printed);
-int printf_reverse(va_list args, int printed);
-int printf_pointer(va_list args, int printed);
+int charsFormats(const char *format, va_list args);
+int _percent(va_list list);
+int _char(va_list list);
+int _string(va_list list);
+int _integer(va_list list);
 
 #endif
